a5d913b1e8cff45cb38f3dfd3c7907077fc618c9
Ulyana <ushinkevich@mail.ru>
2023-07-12 16:53:31 +0400

    реализовать рендер постов из api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { getPosts } from "./api.js";
+import { getPosts, postPosts } from "./api.js";
 import { renderAddPostPageComponent } from "./components/add-post-page-component.js";
 import { renderAuthPageComponent } from "./components/auth-page-component.js";
 import {
@@ -112,6 +112,7 @@ const renderApp = () => {
       onAddPostClick({ description, imageUrl }) {
         // TODO: реализовать добавление поста в API
         console.log("Добавляю пост...", { description, imageUrl });
+        postPosts({ description, imageUrl, token: getToken() });
         goToPage(POSTS_PAGE);
       },
     });
@@ -120,6 +121,7 @@ const renderApp = () => {
   if (page === POSTS_PAGE) {
     return renderPostsPageComponent({
       appEl,
+      token: getToken(),
     });
   }
 
